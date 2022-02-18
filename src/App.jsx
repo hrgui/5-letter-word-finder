5d929f3a56fef399f9d36bcf2b9fd1b9a7307b35
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-02-18 07:53:54 -0800

    fix(App): add negative characters to lookup

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,19 @@ import AutoSizer from "react-virtualized-auto-sizer";
 
 function App() {
   const inputRef = React.useRef(null);
+  const negativeInputRef = React.useRef(null);
   const [_data, setFilteredList] = React.useState(data);
 
   function handleRegexChange() {
-    const value = inputRef.current.value;
-    if (value) {
-      setFilteredList(data.filter((d) => d.match(new RegExp(value, "g"))));
+    const defaultRegexValue = inputRef.current.value;
+    const charactersToExcludeValue = negativeInputRef.current.value;
+
+    if (defaultRegexValue || charactersToExcludeValue) {
+      setFilteredList(
+        data
+          .filter((d) => d.match(new RegExp(defaultRegexValue, "g")))
+          .filter((d) => d.match(new RegExp(`^[^${charactersToExcludeValue}]+$`, "g")))
+      );
     } else {
       setFilteredList(data);
     }
@@ -19,7 +26,7 @@ function App() {
   return (
     <div className="dark:bg-gray-800 w-screen h-screen">
       <div className="pl-8 pr-8 pt-8">
-        <label className="block">
+        <label className="block mb-4">
           <input
             ref={inputRef}
             type="text"
@@ -34,6 +41,23 @@ function App() {
                     focus:border-gray-500 focus:bg-white dark:focus:bg-gray-700 focus:ring-0
                   "
             placeholder="Insert Regular Expression to search list.... (e.g. \w\w\w\wr is 5 letter words that end with r)"
+          />
+        </label>
+        <label className="block">
+          <input
+            ref={negativeInputRef}
+            type="text"
+            defaultValue=""
+            onChange={handleRegexChange}
+            className="
+                    block
+                    w-full
+                    rounded-md
+                    bg-gray-100 dark:bg-gray-600 dark:text-white
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white dark:focus:bg-gray-700 focus:ring-0
+                  "
+            placeholder="Insert characters to exclude"
           />
         </label>
         <div className="h-[calc(100vh-10vh)] overflow-auto bg-gray-50 dark:bg-gray-600 dark:text-white p-2 mt-4 rounded">
