d58dc0c1801a6256f753eb107c13c5ab24a73584
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-02-23 22:38:02 -0800

    feat: Add must have characters to lookup

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import AutoSizer from "react-virtualized-auto-sizer";
 function App() {
   const inputRef = React.useRef(null);
   const negativeInputRef = React.useRef(null);
+  const mustHaveInputRef = React.useRef(null);
   const [_data, setFilteredList] = React.useState(data);
 
   function handleRegexChange() {
     const defaultRegexValue = inputRef.current.value;
     const charactersToExcludeValue = negativeInputRef.current.value;
+    const charactersToMustHaveValue = mustHaveInputRef.current?.value?.split("") || [];
 
-    if (defaultRegexValue || charactersToExcludeValue) {
+    if (defaultRegexValue || charactersToExcludeValue || charactersToMustHaveValue.length) {
       setFilteredList(
         data
           .filter((d) => d.match(new RegExp(defaultRegexValue, "g")))
           .filter((d) => d.match(new RegExp(`^[^${charactersToExcludeValue}]+$`, "g")))
+          .filter((d) => charactersToMustHaveValue.every((c) => d.includes(c)))
       );
     } else {
       setFilteredList(data);
@@ -43,7 +46,7 @@ function App() {
             placeholder="Insert Regular Expression to search list.... (e.g. \w\w\w\wr is 5 letter words that end with r)"
           />
         </label>
-        <label className="block">
+        <label className="block mb-4">
           <input
             ref={negativeInputRef}
             type="text"
@@ -60,7 +63,24 @@ function App() {
             placeholder="Insert characters to exclude"
           />
         </label>
-        <div className="h-[calc(100vh-10vh)] overflow-auto bg-gray-50 dark:bg-gray-600 dark:text-white p-2 mt-4 rounded">
+        <label className="block">
+          <input
+            ref={mustHaveInputRef}
+            type="text"
+            defaultValue=""
+            onChange={handleRegexChange}
+            className="
+                    block
+                    w-full
+                    rounded-md
+                    bg-gray-100 dark:bg-gray-600 dark:text-white
+                    border-transparent
+                    focus:border-gray-500 focus:bg-white dark:focus:bg-gray-700 focus:ring-0
+                  "
+            placeholder="Insert characters must have"
+          />
+        </label>
+        <div className="h-[calc(100vh-220px)] overflow-auto bg-gray-50 dark:bg-gray-600 dark:text-white p-2 mt-4 rounded">
           <AutoSizer>
             {({ height, width }) => (
               <List
