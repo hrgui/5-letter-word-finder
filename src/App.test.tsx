ab9814776a9f7d46568c27c177cf547e38b4dbf3
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-04-17 23:23:44 -0700

    test: fix unit tests to new standard

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -19,19 +19,25 @@ it("should render the default state - nothing filtered", async () => {
   });
 });
 
-it("should filter according to RegExInput if it is set", async () => {
+it("should filter according to BasicInput if it is set (x,x,x,x,r (lock)) => find all 5 letter words that end with r", async () => {
   render(<App />);
   expect(await screen.findByText("aahed")).toBeInTheDocument();
 
-  await userEvent.type(screen.getByTestId("RegExInput"), `\\w\\w\\w\\wr`);
+  await userEvent.type(screen.getByTestId("basic-letter-input-4"), `r`);
+  await userEvent.click(screen.getByTestId("basic-letter-input-4-lock"));
   expect(await screen.findByText("abear")).toBeInTheDocument();
 });
 
-it("should filter according to RegExInput, NegativeInput, MustHaveInput if it is set", async () => {
+it("should filter according to BasicInput and MustHaveInput if it is set to (b (lock),x,o (lock),x,e (lock)), must have s", async () => {
   render(<App />);
   expect(await screen.findByText("aahed")).toBeInTheDocument();
-
-  await userEvent.type(screen.getByTestId("RegExInput"), `b\\wo\\we`);
+  // screen.debug();
+  await userEvent.type(screen.getByTestId("basic-letter-input-0"), `b`);
+  await userEvent.click(screen.getByTestId("basic-letter-input-0-lock"));
+  await userEvent.type(screen.getByTestId("basic-letter-input-2"), `o`);
+  await userEvent.click(screen.getByTestId("basic-letter-input-2-lock"));
+  await userEvent.type(screen.getByTestId("basic-letter-input-4"), `e`);
+  await userEvent.click(screen.getByTestId("basic-letter-input-4-lock"));
   expect(await screen.findByText("biome")).toBeInTheDocument();
   await userEvent.type(screen.getByTestId("MustHaveInput"), `s`);
   expect(await screen.findByText("boose")).toBeInTheDocument();
--- src/components/BasicInput.tsx
+++ src/components/BasicInput.tsx
@@ -2,20 +2,19 @@ import React from "react";
 import BasicLetterInput from "./BasicLetterInput";
 
 type Props = {
-  value?: string;
+  value?: string[];
   onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  onLockChange?;
 };
 
-const BasicInput = ({ value, onChange, onLockChange }: Props) => {
-  const [internalValue, setInternalValue] = React.useState((value || " ".repeat(5)).split(""));
+const BasicInput = ({ value, onChange }: Props) => {
+  const [internalValue, setInternalValue] = React.useState(value || new Array(5).fill(""));
   const [internalLocks, setInternalLocks] = React.useState(
-    (value || " ".repeat(5)).split("").map((v) => false)
+    value?.map(() => false) || new Array(5).fill(() => false)
   );
 
   React.useEffect(() => {
-    if (value !== internalValue.join("")) {
-      setInternalValue((value || " ".repeat(5)).split(""));
+    if (value && value.join("") !== internalValue.join("")) {
+      setInternalValue(value);
     }
   }, [value]);
 
@@ -26,10 +25,8 @@ const BasicInput = ({ value, onChange, onLockChange }: Props) => {
   }
 
   function handleChangeInPosition(pos, newValue) {
-    console.log(pos, newValue);
     const parts = [...internalValue];
     parts[pos] = newValue;
-    console.log(parts);
     setInternalValue(parts);
     callOnChange(parts, internalLocks);
   }
@@ -44,30 +41,35 @@ const BasicInput = ({ value, onChange, onLockChange }: Props) => {
   return (
     <label className="grid grid-cols-5 mb-4">
       <BasicLetterInput
+        id="basic-letter-input-0"
         value={internalValue[0]}
         lockValue={internalLocks[0]}
         onLockChange={(e) => handleInternalLocksChange(0, e.target.checked)}
         onChange={(e) => handleChangeInPosition(0, e.target.value)}
       />
       <BasicLetterInput
+        id="basic-letter-input-1"
         value={internalValue[1]}
         lockValue={internalLocks[1]}
         onLockChange={(e) => handleInternalLocksChange(1, e.target.checked)}
         onChange={(e) => handleChangeInPosition(1, e.target.value)}
       />
       <BasicLetterInput
+        id="basic-letter-input-2"
         value={internalValue[2]}
         lockValue={internalLocks[2]}
         onLockChange={(e) => handleInternalLocksChange(2, e.target.checked)}
         onChange={(e) => handleChangeInPosition(2, e.target.value)}
       />
       <BasicLetterInput
+        id="basic-letter-input-3"
         value={internalValue[3]}
         lockValue={internalLocks[3]}
         onLockChange={(e) => handleInternalLocksChange(3, e.target.checked)}
         onChange={(e) => handleChangeInPosition(3, e.target.value)}
       />
       <BasicLetterInput
+        id="basic-letter-input-4"
         value={internalValue[4]}
         lockValue={internalLocks[4]}
         onLockChange={(e) => handleInternalLocksChange(4, e.target.checked)}
