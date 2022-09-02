49fa74f92f529d00093077c59c4814e5fea5cda6
Harman Goei <5001432+hrgui@users.noreply.github.com>
2022-09-02 13:44:27 -0700

    test(QuickAddWordForm): fix the test description to be less generic

--- src/components/QuickAddWordForm.test.tsx
+++ src/components/QuickAddWordForm.test.tsx
@@ -1,7 +1,8 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import QuickAddWordForm from "./QuickAddWordForm";
-it("should render without crashing", async () => {
+
+it("should be able to hit ENTER to submit upon quickly adding a word", async () => {
   const handleSubmit = vi.fn();
   render(<QuickAddWordForm onSubmit={handleSubmit} />);
   const input = screen.getByPlaceholderText(/Quick add word/);
